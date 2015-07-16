e82695a8fb7d640e1fae8f3fb739f88fb0479a92
David McPeek <david.mcpeek@yale.edu>
2015-07-16 00:07:33 -0400

    lose a few comments

--- application.macosx/source/Cumulative_Distribution_Simulation.java
+++ application.macosx/source/Cumulative_Distribution_Simulation.java
@@ -23,11 +23,6 @@ public class Cumulative_Distribution_Simulation extends PApplet {
 // reach 1.0 because there is still a non-negligible chance of an occurence outside of the
 // x-axis bounds (on the outer levels of the standard deviation)
 
-// features to add:
-// 1. (done) the ability to alter the standard deviation. The plots should scale 
-//    accordingly. 
-// 2. code readability for reproducibility
-// 3. rewrite code to work outside of the KA development environment; use it as a showcase on //    Github
 
 int axis_x = 140;  // where the x-axis will be located for top graph
 int axis_y = 200;
